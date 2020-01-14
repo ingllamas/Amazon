7798447b2582a724b41d623fd073eb6ce0a112a3
DobleL <doble-l@outlook.com>
2020-01-14 00:38:19 -0600

    Minimize javascript in Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,15 @@
-const { series } = require('gulp');
+const {src, dest, series } = require('gulp');
 const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+const minimizeJavascript = () =>
+{
+    return src('dev/js/*.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(dest('public/js'));
+}
+
+exports.default = series(minimizeJavascript);
